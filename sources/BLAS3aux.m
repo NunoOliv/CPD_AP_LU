2079c2533b6ae8d8d2c75a80efbf9d1f5f1ee702
RafaelAnt <rafael.uminho@hotmail.com>
2016-05-10 18:13:35 +0100

    Blas3 fix?

--- sources/BLAS3aux.m
+++ sources/BLAS3aux.m
@@ -1,23 +1,23 @@
-function B=BLAS3aux(B,j,o,last)
+function A=BLAS3aux(A,j,o,last)
 
 %A(i:n,i:last)=BLAS3LU(A(i:n,i:last)); % step 1 (L22, L32)
 
-A(B(j:o,j:last));
+%A=B(j:o,j:last);
 
-[m n]=size(A);
+m = j-o;
+n = j-last;
 for i=1:min(m-1,n)
+    i2=i+j;
     % apply row permutations to A and L
-    [M,I] = max(A(i:n,i));
-    linha = I+i-1;
-    if(linha > i)
-        A=swapLine(A,linha,i);
-        B=swapLine(B,linha,i);
+    [M,I] = max(A(i2:n,i2));
+    linha = I+i2-1;
+    if(linha > i2)
+        A=swapLine(A,linha,i2);
+        %B=swapLine(B,linha+j-1,i+j-1);
     end
     
-    A(i+1:m,i)=A(i+1:m,i)/A(i,i);
-    if i<n
-        A(i+1:m,i+1:n)=A(i+1:m,i+1:n)-A(i+1:m,i)*A(i,i+1:n);
+    A(i2+1:m,i2)=A(i2+1:m,i2)/A(i2,i2);
+    if i2<n
+        A(i2+1:m,i2+1:n)=A(i2+1:m,i2+1:n)-A(i2+1:m,i2)*A(i2,i2+1:n);
     end
 end
-
-B(j:o,j:last)=A;
