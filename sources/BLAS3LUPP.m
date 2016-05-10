6375a6279f4c1b67473a1effd87d220aa744d3b0
RafaelAnt <rafael.uminho@hotmail.com>
2016-05-10 17:13:59 +0100

    BLAS3 Aux added

--- sources/BLAS3LUPP.m
+++ sources/BLAS3LUPP.m
@@ -1,23 +1,12 @@
 function A=BLAS3LUPP(A,b)
-% Block LU factorization with partial pivoting, overwriting L and U on A
-% see ALGORITHM 2.10 in Applied Numerical Linear Algebra, J. Demmel, SIAM
-% (2007), p.74. Size of blocks is b.
-% Uses BLAS2LU to produce the LU dec. of rectangular submatrices of A
-
 
 n=length(A);
 for i=1:b:n-1
     % apply row permutations to A and L
     
-    [M,I] = max(A(i:n,i));
-    linha = I+i-1;
-    if(linha > i)
-        A=swapLine(A,linha,i);
-    end
-    
-    
     last=min(i+b-1,n);  
-    A(i:n,i:last)=BLAS2LU(A(i:n,i:last)); % step 1 (L22, L32)
+    
+    A=BLAS3aux(A,i,n,last); % step 1 (L22, L32)
     if n-i+1 > b  % SIZE OF REMAINING BLOCK LARGER THAN b
         L22=tril(A(i:last,i:last),-1)+eye(b);
         A(i:last,i+b:n)=inv(L22)*A(i:last,i+b:n); % step 2 (U22)
