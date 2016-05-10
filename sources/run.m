7ad11cb905afcc22b1cf8e32e1bec3763349a84a
RafaelAnt <rafael.uminho@hotmail.com>
2016-05-10 15:29:27 +0100

    Error Calculation FIxed

--- sources/run.m
+++ sources/run.m
@@ -1,16 +1,22 @@
 clear all
 
-%A = [ 3 2 4; 8 4 2; 3 5 1];
-%A = [ 2 7 9; 5 7 9; 1 4 5];
+% Test For:
+% 100
+% 300
+% 500
+% 700
+% 900
 
-A = rand(10);
+A = rand(100);
 
+tic;
 %B = BLAS2LU(A);
 B = BLAS2LUPP(A);
 
 %B = BLAS3LU(A,1);
 %B = BLAS3LUPP(A,1);
+toc
 
 [L,U,P] = lu(A);
 
-Error = norm(P*B - L*U)
+Error = norm(P*A - L*U)
